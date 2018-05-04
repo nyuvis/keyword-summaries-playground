99d5927a75c618a52e26881276e8163204eccd63
BMichela <michela.bragagnolo18@gmail.com>
2018-05-04 19:24:37 -0400

    scores

---
+++ src/scores.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+
+class Score extends Component {
+    render() {
+        return (
+            <div className="score">
+            <p> style={{ color: "red" }}> scores </p>
+            </div>
+        );
+    }
+}
+
+export default Score;
+
+
