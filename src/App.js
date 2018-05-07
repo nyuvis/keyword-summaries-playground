5257dc4a0c370ed897b51d463c404b5f92168620
Cristian Felix <cristian.felix@gmail.com>
2018-05-07 13:38:49 -0400

    Add Api details

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React, { Component } from "react";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
 
-import Body from "./scores";
+import Body from "./Body";
+
+const datasetID = 2;
 
 //Query to get list of categories
 const basicData = gql`
-  query basicData {
-    Dataset(ID: 19) {
+  query basicData($id: ID) {
+    Dataset(ID: $id) {
       Select {
         Size
-        Categories: Values(field: "categories", size: 20) {
+        Categories: Values(field: "categories", size: 50) {
           Key
         }
       }
@@ -20,12 +22,14 @@ const basicData = gql`
 
 class App extends Component {
   render() {
+    let variables = {
+      id: datasetID
+    };
     return (
-      <Query query={basicData}>
+      <Query query={basicData} variables={variables}>
         {result => {
           const { data } = result;
           if (!data.Dataset) return <div>Loading</div>; //Checking if the data is loaded
-
           return <Body categories={data.Dataset.Select.Categories} />; //Returning the Body.js component
         }}
       </Query>
--- src/Body.js
+++ src/Body.js
@@ -1,15 +1,28 @@
 import React, { PureComponent } from "react";
 import gql from "graphql-tag";
 import { Query } from "react-apollo";
+import AceEditor from "react-ace";
+import "brace/mode/json";
+import "brace/theme/github";
+import { sortBy, random } from "lodash";
+import { extent } from "d3-array";
 
 const loadData = gql`
-  query getData($category: [String], $size: Int) {
-    Dataset(ID: 19) {
+  query getData($category: [String], $number: Int, $discriminant: Boolean) {
+    Dataset(ID: 2) {
+      Select {
+        Values(field: "text", size: $number) {
+          Key
+          Score
+          Count
+        }
+      }
       Keywords: Select(filter: { field: "categories", in: $category }) {
         Size
-        Values(field: "text", discriminant: true, size: $size) {
+        Values(field: "text", discriminant: $discriminant, size: $number) {
           Key
-          Stat
+          Score
+          Count
         }
       }
     }
@@ -17,34 +30,97 @@ const loadData = gql`
 `;
 
 class Body extends PureComponent {
+  constructor() {
+    super();
+    this.state = {
+      params: {
+        number: 20,
+        discriminant: true,
+        category: "Italian",
+        sortby: "Key",
+        noise: 0
+      }
+    };
+  }
+  onChange = (value, v3, c, d) => {
+    try {
+      let params = JSON.parse(value);
+      this.setState({
+        params: params
+      });
+    } catch (error) {
+      console.log("Invalid");
+    }
+  };
   render() {
     const { categories } = this.props;
-
+    const { params } = this.state;
     return (
-      <div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0
+        }}
+      >
         <div>
-          <select>
-            {categories.map(
+          <select
+            value={this.state.params.category}
+            onChange={e =>
+              this.setState({
+                params: { ...this.state.params, category: e.target.value }
+              })
+            }
+          >
+            {sortBy(categories, "Key").map(
               d => <option key={d.Key}>{d.Key}</option> //Key is the name of the category
             )}
           </select>
-          {/* Load list of keywords----------------------------------------- */}
+        </div>
+        <div style={{ display: "flex", flex: "1" }}>
           <div>
-            <Query
-              query={loadData}
-              variables={{ category: "Steakhouses", size: 20 }}
-            >
+            <AceEditor
+              mode="json"
+              value={JSON.stringify(this.state.params, null, 4)}
+              theme="github"
+              name="UNIQUE_ID_OF_DIV"
+              debounceChangePeriod={1000}
+              editorProps={{ $blockScrolling: true }}
+              onChange={this.onChange}
+            />
+          </div>
+          <div style={{ overflow: "auto" }}>
+            <Query query={loadData} variables={this.state.params}>
               {result => {
                 const { data } = result;
                 if (!data.Dataset) return <div>Loading</div>; //Checking if the data is loaded
 
+                //Prepare Words
+                let keywords = data.Dataset.Keywords.Values;
+                let noise = data.Dataset.Select.Values;
+
+                let numKeywords = keywords.length;
+                let numNoise = Math.floor(params.noise * numKeywords);
+                numKeywords = numKeywords - numNoise;
+                keywords = keywords.slice(0, numKeywords);
+                if (params.sortby !== "Key") {
+                  let valueExtent = extent(keywords, d => d[params.sortby]);
+                  noise = noise.map(d => ({
+                    ...d,
+                    [params.sortby]: random(...valueExtent)
+                  }));
+                }
+                keywords = [...keywords, ...noise.slice(0, numNoise)];
+
+                keywords = sortBy(keywords, [params.sortby]);
                 //Display Keywords
+
                 return (
-                  <div>
-                    {data.Dataset.Keywords.Values.map(d => (
-                      <div key={d.Key}>{d.Key}</div>
-                    ))}
-                  </div>
+                  <div>{keywords.map(d => <div key={d.Key}>{d.Key}</div>)}</div>
                 );
               }}
             </Query>
